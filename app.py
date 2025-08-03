d319fa57a6a3720da67c85005c2c0cbc35c47455
Eleanor <157729330+leekk@users.noreply.github.com>
2025-08-03 13:00:25 -0400

    testing calendar god knows how this is gonna turn out

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import json
 # I'm having heart palpations rn haha
 import os
 from huggingface_hub import InferenceClient
+from streamlit_calendar import calendar
 
 # -------------------- TRYNG AI HERE --------------------
 os.environ["HF_TOKEN"] = st.secrets['HF_TOKEN']
@@ -154,7 +155,7 @@ with st.sidebar:
         st.markdown("Tip: Breathe in for 4 seconds...")
 
 
-# -------------------- UI EXTRA --------------------
+# -------------------- UI EXTRA(TESTING BGS) --------------------
 st.markdown(
     """
     <style>
@@ -182,3 +183,68 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+# -------------------- CALENDAR N STUFF --------------------
+
+calendar_options = {
+    "editable": True,
+    "selectable": True,
+    "headerToolbar": {
+        "left": "today prev,next",
+        "center": "title",
+        "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+    },
+    "slotMinTime": "06:00:00",
+    "slotMaxTime": "18:00:00",
+    "initialView": "resourceTimelineDay",
+    "resourceGroupField": "building",
+    "resources": [
+        {"id": "a", "building": "Building A", "title": "Building A"},
+        {"id": "b", "building": "Building A", "title": "Building B"},
+        {"id": "c", "building": "Building B", "title": "Building C"},
+        {"id": "d", "building": "Building B", "title": "Building D"},
+        {"id": "e", "building": "Building C", "title": "Building E"},
+        {"id": "f", "building": "Building C", "title": "Building F"},
+    ],
+}
+calendar_events = [
+    {
+        "title": "Event 1",
+        "start": "2023-07-31T08:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 2",
+        "start": "2023-07-31T07:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 3",
+        "start": "2023-07-31T10:40:00",
+        "end": "2023-07-31T12:30:00",
+        "resourceId": "a",
+    }
+]
+custom_css="""
+    .fc-event-past {
+        opacity: 0.8;
+    }
+    .fc-event-time {
+        font-style: italic;
+    }
+    .fc-event-title {
+        font-weight: 700;
+    }
+    .fc-toolbar-title {
+        font-size: 2rem;
+    }
+"""
+
+calendar = calendar(
+    events=calendar_events,
+    options=calendar_options,
+    custom_css=custom_css,
+    key='calendar', # Assign a widget key to prevent state loss
+    )
+st.write(calendar)
